324a294d085ea1bc21863c77a1ab26e0bbb02d39
Jason Tremper <jason@mechavian.com>
2018-01-21 16:28:56 -0500

    Simplifying LED test

--- 001-blinkled/index.js
+++ 001-blinkled/index.js
@@ -1,7 +1,28 @@
-var Gpio = require('onoff').Gpio,
-  led = new Gpio(17, 'out'),
-  button = new Gpio(4, 'in', 'both');
+import { setInterval, clearInterval } from 'timers';
 
-button.watch(function(err, value) {
-  led.writeSync(value);
-});
+const Gpio = require('onoff').Gpio,
+      led = new Gpio(17, 'out');
+
+var count = 10;
+var set = false;
+
+setLed(false);
+
+const timer = setInterval(() => {
+    if (count-- > 0) {
+        setLed(set = !set);
+    } else {
+        setLed(false);
+        clearInterval(timer);
+    }
+}, 500);
+
+function setLed(led, value) {
+    if (value) {
+        console.log('LED ON');
+        led.writeSync(1);
+    } else {
+        console.log('LED OFF');
+        led.writeSync(0);        
+    }
+}
